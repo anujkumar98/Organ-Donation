a641cc99f5190667ea5c892dbb0f247cd5cbb909
Anuj Kumar <anujkumar@Anujs-MacBook-Pro.local>
2022-11-28 02:44:19 -0500

    Added Enterprise

---
+++ Organ Donation/src/Business/Enterprise/Enterprise.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import Business.Network.Network;
+
+/**
+ *
+ * @author anujkumar
+ */
+public class Enterprise extends Network{
+    
+    private EnterpriseType enterpriseType;
+    private long contact;
+    private String email;
+    private int zipcode;
+    
+    public Enterprise(EnterpriseType enterpriseType){
+        this.enterpriseType=enterpriseType;
+    }
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    public enum EnterpriseType{
+        Hospital("Hospital"),
+        OPO("OPO"),
+        NGO("NGO"),
+        Transport("Transport");
+        
+        
+        private String value;
+        
+        private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
+        @Override
+        public String toString(){
+        return value;
+    }
+    }
+}
---
+++ Organ Donation/src/Business/Network/Network.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Network;
+
+/**
+ *
+ * @author anujkumar
+ */
+public class Network {
+    private String name;
+    private String city;
+    private String state;
+    private String region;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    
+    
+    //private Enterprise_Directory Enterprise_Directory;
+    
+    
+}
